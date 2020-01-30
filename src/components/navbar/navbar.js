ca826436a8a7af6bfdfcfe3f70f83b739c0b545e
brandiherrera <bcjherrera@gmail.com>
2020-01-30 12:32:34 -0800

    logout implemented, navbar toggle implemented with login/logout status

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -62,29 +62,36 @@ export default class Navbar extends React.Component {
     render() {
         return (
             <nav className='nav' id='nav'>
-                <div className='Header__not-logged-in'>
-                    <NavLink
-                        exact to='/'
-                        className='nav-link-home'
-                        id='home'>
-                        MUSEME
-                        {/* TODO - logo */}
-                    </NavLink>
-                <div className='Header__right'>
-                    <NavLink
-                        className='nav-link signup'
-                        to='/signup'>
-                        Sign Up
-                    </NavLink>
-                    <NavLink
-                        className='nav-link login'
-                        to='/login'>
-                        Log in
-                    </NavLink>
-                </div>
-{/* MOVE THIS UP TO OTHER DIV? */}
-            </div>
+                {TokenService.hasAuthToken()
+                    ? this.renderLogoutLink()
+                    : this.renderLoginLink()}
             </nav>
         )
+//         return (
+//             <nav className='nav' id='nav'>
+//                 <div className='Header__not-logged-in'>
+//                     <NavLink
+//                         exact to='/'
+//                         className='nav-link-home'
+//                         id='home'>
+//                         MUSEME
+//                         {/* TODO - logo */}
+//                     </NavLink>
+//                 <div className='Header__right'>
+//                     <NavLink
+//                         className='nav-link signup'
+//                         to='/signup'>
+//                         Sign Up
+//                     </NavLink>
+//                     <NavLink
+//                         className='nav-link login'
+//                         to='/login'>
+//                         Log in
+//                     </NavLink>
+//                 </div>
+// {/* MOVE THIS UP TO OTHER DIV? */}
+//             </div>
+//             </nav>
+//         )
     }
 }
