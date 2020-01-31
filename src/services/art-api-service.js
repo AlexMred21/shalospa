34a3f226ac742b8de2ebf4e7a6492ab57383d696
brandiherrera <bcjherrera@gmail.com>
2020-01-30 18:12:06 -0800

    updated to use server for fetch requests

--- src/services/art-api-service.js
+++ src/services/art-api-service.js
@@ -4,9 +4,9 @@ import config from '../config';
 const ArtApiService = {
   getArtGallery() {
     return fetch(`${config.API_ENDPOINT}/art`, {
-    //   headers: {
-    //     'authorization': `bearer ${TokenService.getAuthToken()}`,
-    //   },
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
     })
       .then(res =>
         (!res.ok)
