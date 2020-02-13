a7e4e30964772005371f320c58005717cf65144b
brandiherrera <bcjherrera@gmail.com>
2020-02-13 15:30:45 -0800

    updated config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
     // API_ENDPOINT: `http://localhost:8000/api`,
-    API_ENDPOINT: "https://intense-hamlet-30325.herokuapp.com/api",
+    // API_ENDPOINT: "https://intense-hamlet-30325.herokuapp.com/api",
     // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || process.env.ALT_ENDPOINT,
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
     TOKEN_KEY: 'museum-app-client-auth-token',
 }
