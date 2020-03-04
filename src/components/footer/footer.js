16d10b00d2bff9212a82342395a4b562d2befbf6
brandiherrera <bcjherrera@gmail.com>
2020-03-04 15:38:12 -0800

    updated contact links, added portfolio

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,10 +7,11 @@ export default class Footer extends React.Component {
     render() {
         return (
             <div className='app-footer'>
-                ©2020 Brandi Herrera 
+                <p>©2020 Brandi Herrera</p>
+                    <a href="http://my-web-developer.com"><img src="https://image.flaticon.com/icons/svg/455/455691.svg" alt="portfolio link" className="social" /></a>
                     <a href="https://github.com/brandiherrera"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="github logo" className="social" /></a>
                     <a href="mailto:bcjherrera@gmail.com?Subject=Portfolio Contact" target="_blank"><img src="https://image.flaticon.com/icons/svg/561/561127.svg" alt="email icon" className="social" /></a>
-                    <a href="https://www.linkedin.com/in/brandiherrera1"><img src="https://image.flaticon.com/icons/svg/49/49068.svg" alt="linkedin logo" className="social" /></a>
+                    <a href="https://www.linkedin.com/in/brandi-c-herrera"><img src="https://image.flaticon.com/icons/svg/49/49068.svg" alt="linkedin logo" className="social" /></a>
             </div>
         )
     }
