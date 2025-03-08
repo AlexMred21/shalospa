d148e8a06b57f129e39ccccf04505959e2febfe3
Sheila Alconchel <sheila6av@gmail.com>
2025-03-08 21:48:42 +0100

    Modificaciones de Sheila en footer.js
    He eliminado los enlaces originales y he cambiado los derechos de copyright.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,11 +7,7 @@ export default class Footer extends React.Component {
     render() {
         return (
             <div className='app-footer'>
-                <p>©2020 Brandi Herrera</p>
-                    <a href="http://my-web-developer.com"><img src="https://image.flaticon.com/icons/svg/455/455691.svg" alt="portfolio link" className="social" /></a>
-                    <a href="https://github.com/brandiherrera"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="github logo" className="social" /></a>
-                    <a href="mailto:bcjherrera@gmail.com?Subject=Portfolio Contact" target="_blank"><img src="https://image.flaticon.com/icons/svg/561/561127.svg" alt="email icon" className="social" /></a>
-                    <a href="https://www.linkedin.com/in/brandi-c-herrera"><img src="https://image.flaticon.com/icons/svg/49/49068.svg" alt="linkedin logo" className="social" /></a>
+                <p>©2025 shalospa | Derechos reservados</p>
             </div>
         )
     }
